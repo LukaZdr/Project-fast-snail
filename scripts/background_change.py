3a0142ccdee41e91c6a8c858c40922b9d3034321
LukaZdr <luka.z@hotmail.de>
2019-12-09 22:23:07 +0100

    renames file and adds todo

--- scripts/background_change.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-'''TODO infest some time in tooking up things'''
---
+++ scripts/prepare_image.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+# TODO 1 take background out of given image and replace it with random background
+# TODO 2 for CNN all images need to have the same size
