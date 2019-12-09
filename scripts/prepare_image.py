e088551755b306a00ca5ae7ce8dff7837f90c0d2
LukaZdr <luka.z@hotmail.de>
2019-12-09 23:05:20 +0100

    wip otsu

--- scripts/prepare_image.py
+++ scripts/prepare_image.py
@@ -2,3 +2,11 @@
 
 # TODO 1 take background out of given image and replace it with random background
 # TODO 2 for CNN all images need to have the same size
+
+import numpy as np
+from skimage.io import filters
+
+def binarize_image(img):
+  otsu_threshold = filters.threshold_otsu(img)
+  mask = img < otsu_threshold
+  mask.astype(np.int)
