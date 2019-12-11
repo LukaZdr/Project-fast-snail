e18f42d9ef8e1784ae17c0673150df9c7f03b5d7
Hauke Hoerhold <7hoerhol@informatik.uni-hamburg.de>
2019-12-11 16:29:35 +0100

    save_images_to_npz_file.py script  added

---
+++ scripts/save_images_to_npz_file/save_images_to_npz_file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import skimage.io as io
+
+# Import all Images
+eth = io.ImageCollection("./ethernet/*")       # Label 1
+vga = io.ImageCollection("./vga/*")            # Label 2
+ps2 = io.ImageCollection("./ps2/*")            # Label 3
+co3 = io.ImageCollection("./3comp/*")          # Label 4
+
+# Put images in array with label
+arrayOfImages = []
+labels = []
+
+for img in eth:
+    arrayOfImages.append(img)
+    labels.append(1)
+for img in vga:
+    arrayOfImages.append(img)
+    labels.append(2)
+for img in ps2:
+    arrayOfImages.append(img)
+    labels.append(3)
+for img in co3:
+    arrayOfImages.append(img)
+    labels.append(4)
+   
+np.savez("outfile", data = arrayOfImages, labels = labels)
