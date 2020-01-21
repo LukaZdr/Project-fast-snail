436806f1ccf576f6053265627538c23b55b3b7ac
Hauke <haukefb@gmx.de>
2020-01-21 01:54:37 +0100

    manual approach edited

--- manual_approach/manueller_ansatz_auto_csv.py
+++ manual_approach/manueller_ansatz_auto_csv.py
@@ -195,9 +195,9 @@ tr_shuffled_images, tr_shuffled_labels = shuffle(tr_images, tr_labels)
 va_shuffled_images, va_shuffled_labels = shuffle(va_images, va_labels)
 
 # Write header
-with open("Results.csv", mode="a", newline="\n", encoding="utf-8") as file:
-    file_writer = writer(file, delimiter=';', quotechar='"', quoting=QUOTE_MINIMAL)
-    file_writer.writerow(["distance_measure", "neighbour_count", "descriptor_1", "descriptor_2", "weight", "bin_count", "guessing_accuracy", "time_needed", "image_set"])
+#with open("Results.csv", mode="a", newline="\n", encoding="utf-8") as file:
+#    file_writer = writer(file, delimiter=';', quotechar='"', quoting=QUOTE_MINIMAL)
+#    file_writer.writerow(["distance_measure", "neighbour_count", "descriptor_1", "descriptor_2", "weight", "bin_count", "guessing_accuracy", "time_needed", "image_set"])
 
 ###############################################################################
 # Run function with different settings 
