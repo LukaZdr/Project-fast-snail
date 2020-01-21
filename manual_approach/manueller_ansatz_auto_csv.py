bbbd7a6caa0b72d2482af53a1c3172863431bfad
Hauke <haukefb@gmx.de>
2020-01-21 02:52:37 +0100

    manual approach edited

--- manual_approach/manueller_ansatz_auto_csv.py
+++ manual_approach/manueller_ansatz_auto_csv.py
@@ -158,7 +158,7 @@ def calculate_estimated_labels(p_distance_measure, p_neighbour_count, p_descript
     # Calculate guessing accuracy
     guessing_accuracy = guessing_accuracy(estimated_labels, va_shuffled_labels)
     
-    # Write settings to file
+    # Write settings and result to file
     with open("Results.csv", mode="a", newline="\n", encoding="utf-8") as file:
         file_writer = writer(file, delimiter=";", quotechar="'", quoting=QUOTE_MINIMAL)
         file_writer.writerow([distance_measure, str(neighbour_count), descriptor_1, descriptor_2, str(weight), str(bin_count), "{:.2f}".format(guessing_accuracy), "{:.2f}".format(needed_time), str(image_set)])
