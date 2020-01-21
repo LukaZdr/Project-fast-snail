90be29c340bd8574a5726d91f3b608edcdb64240
Hauke <haukefb@gmx.de>
2020-01-21 18:41:33 +0100

    manual approach edited

--- manual_approach/csv_to_html_table.py
+++ manual_approach/csv_to_html_table.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 csv = open("Results2.csv","r")
-html = open("table_for_viewing_result/index2.html", "w")
+html = open("table_for_viewing_result/index.html", "w")
 
 html.write("<html lang=\"de\"><head><meta charset=\"utf-8\"><title>CV - Manueller Ansatz</title><style>body{font-family:sans-serif}thead th{text-align:left;padding:10 8 !important}td{border-left:1px solid #ccc}td:first-child{border-left:none}table.dataTable.stripe tbody tr.odd, table.dataTable.display tbody tr.odd{background-color:#fff !important}table.dataTable.stripe tbody tr.even, table.dataTable.display tbody tr.even{background-color:#ddd !important}table.dataTable.display tbody tr.odd > .sorting_1, table.dataTable.order-column.stripe tbody tr.odd>.sorting_1{background-color:#daffd9 !important}table.dataTable.display tbody tr.even > .sorting_1, table.dataTable.order-column.stripe tbody tr.even>.sorting_1{background-color:#a6eda4 !important}table.dataTable.hover tbody tr:hover, table.dataTable.display tbody tr:hover, table.dataTable.display tbody tr:hover > .sorting_1, table.dataTable.order-column.hover tbody tr:hover>.sorting_1{background-color:#b8bfff !important}</style><link rel=\"stylesheet\" href=\"assets/jquery.dataTables.min.css\"> <script src=\"assets/jquery-3.3.1.js\"></script> <script src=\"assets/jquery.dataTables.min.js\"></script> </head>")
 html.write("<body><h1>Manual approach results</h1><table id='t' class='display'><thead><tr><th title='Field #1'>distance_measure</th><th title='Field #2'>neighbour_count</th><th title='Field #3'>descriptor_1</th><th title='Field #4'>descriptor_2</th><th title='Field #5'>weight</th><th title='Field #6'>bin_count</th><th title='Field #7'>guessing_accuracy (%)</th><th title='Field #8'>time_needed (min)</th><th title='Field #9'>image_set</th></tr></thead><tbody>")
@@ -37,4 +37,3 @@ html.write("<script>$(document).ready(function(){$('#t').DataTable({\"lengthMenu
 html.close()
 
 print("\nDone!")
-print("!!!")
